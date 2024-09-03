9b1e9f9d02c09bfdd66f5e09c058a7e68116a673
leony <leo.ny2010@hotmail.com>
2024-09-03 19:03:11 -0300

    adicionando soft skills

--- modal/modal.js
+++ modal/modal.js
@@ -1,23 +1,23 @@
-// Obtém o modal
+
 var modal = document.getElementById("myModal");
 
-// Obtém o botão que abre o modal
+
 var btn = document.getElementById("openModalBtn");
 
-// Obtém o elemento <span> que fecha o modal
+l
 var span = document.getElementsByClassName("close")[0];
 
-// Quando o usuário clicar no botão, abre o modal
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// Quando o usuário clicar em <span> (x), fecha o modal
+
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// Quando o usuário clicar fora do modal, fecha o modal
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
